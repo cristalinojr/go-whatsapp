3569c6121d71c883fcfa2a124daae737646c408c
Cristalino Jr <46726187+cristalinojr@users.noreply.github.com>
2020-11-08 20:08:40 -0300

    Update message.go
    test

--- message.go
+++ message.go
@@ -10,8 +10,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/Rhymen/go-whatsapp/binary"
-	"github.com/Rhymen/go-whatsapp/binary/proto"
+	"github.com/cristalinojr/go-whatsapp/binary"
+	"github.com/cristalinojr/go-whatsapp/binary/proto"
 )
 
 type MediaType string
