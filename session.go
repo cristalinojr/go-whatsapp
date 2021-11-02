61a66a71266587fd131fcbb8e1c40f53239b3ee7
iileandro <iileandro@gmail.com>
2021-11-02 01:14:08 -0300

    Update session.go
    V 2.2142.12

--- session.go
+++ session.go
@@ -18,7 +18,7 @@ import (
 )
 
 //represents the WhatsAppWeb client version
-var waVersion = []int{2, 2045, 15}
+var waVersion = []int{2, 2142, 12}
 
 /*
 Session contains session individual information. To be able to resume the connection without scanning the qr code
